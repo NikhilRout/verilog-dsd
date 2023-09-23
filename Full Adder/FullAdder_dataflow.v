b1fd6b2e9d9293436f8afed5a344990980b5cd82
Nikhil Rout <135248190+NikhilRout@users.noreply.github.com>
2023-09-24 02:06:27 +0530

    Deleted Full Adder directory

--- Full Adder/FullAdder_dataflow.v
+++
@@ -1,6 +0,0 @@
-module FullAdder_dataflow(sum,carry,a,b,Cin);
-  output sum,carry;
-  input a,b,Cin;
-  assign sum = a^b^Cin;
-  assign carry  = (a&b)|(Cin&(a|b)); //alternatively (a&b)|(Cin&a)|(Cin&b)
-endmodule
