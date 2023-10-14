b77f27f638036f6fb00c8f9934c7f1a10cc8248e
Nikhil Rout <135248190+NikhilRout@users.noreply.github.com>
2023-10-14 16:14:40 +0530

    Update SR_Latch_testbench.v

--- SR Latch/SR_Latch_testbench.v
+++ SR Latch/SR_Latch_testbench.v
@@ -4,7 +4,7 @@ module SR_Latch_testbench;
   SR_Latch_dataflow uut (.R(R),.S(S),.EN(EN), .Q(Q), .Qbar(Qbar));
   initial begin
     $monitor("EN=%b R=%b S=%b, Q=%b, Qbar=%b", EN, R, S, Q,Qbar);
-    #100 EN = 1; R = 0; S = 0;
+    EN = 1; R = 0; S = 0;
     #100 R = 0; S = 1;
     #100 R = 0; S = 0; //holds previous state (latch)
     #100 R = 1; S = 0;
