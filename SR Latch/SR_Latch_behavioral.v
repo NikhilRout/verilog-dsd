33f4d1ebb53764eab613909c7e725f90f48d81bf
Nikhil Rout <135248190+NikhilRout@users.noreply.github.com>
2023-10-14 19:35:18 +0530

    Updated SR_Latch_behavioral.v

--- SR Latch/SR_Latch_behavioral.v
+++ SR Latch/SR_Latch_behavioral.v
@@ -2,7 +2,7 @@ module SR_Latch_behavioral(
     input R, S, EN,
     output reg Q, Qbar
 );
-  always @(*) begin
+    always @(EN) begin
     if (EN) begin
       Q <= ~(R | Qbar);
       Qbar <= ~(S | Q);
