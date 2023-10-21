8918f27aa9d384abd5011dcf3e9e1f379376d431
NikhilRout <nikhilrout97@gmail.com>
2023-10-21 22:30:55 +0530

    improved SIPO

--- SIPO/SIPO.v
+++ SIPO/SIPO.v
@@ -4,15 +4,12 @@ module SIPO(
   input clk, rst, data_in,
   output reg [3:0] data_out
 );
-  reg [3:0] tmp;
   always @(posedge clk or posedge rst) begin
     if (rst) begin
-      tmp <= 4'b0000;
       data_out <= 4'b0000;
     end else begin
-      tmp = tmp >> 1;
-      tmp[3] = data_in;
-      data_out <= tmp;
+      data_out = data_out >> 1;
+      data_out[3] = data_in;
     end
   end
 endmodule
